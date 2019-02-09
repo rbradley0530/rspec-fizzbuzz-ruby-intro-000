b3febcc4bdfd6527d1d1c9c9074dcbfce2762bd0
Ryan Bradley <ryanbradley@kpmg.com>
2019-02-09 22:59:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-fizz_3 = fizzbuzz(3)
-fizz_5 = fizzbuzz(5)
 
-
-
-def fizzbuzz
+def fizzbuzz(fizz_3)
   #if
   #  puts "fizz"
   #elsif
@@ -16,3 +12,6 @@ def fizzbuzz
   #else
   #  puts "nil"
 end
+
+fizz_3 = fizzbuzz(3)
+fizz_5 = fizzbuzz(5)
