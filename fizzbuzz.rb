2721e01d502b27f4815f273005863178e14dee1a
Ryan Bradley <ryanbradley@kpmg.com>
2019-02-09 22:57:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(value)
-  if 'value'/3 = true
-    puts "fizz"
-  elsif '#{value}'/5 = true
-    puts "buzz"
-  elsif '#{value}'/(3 && 5) = true
-    puts "fizzbuzz"
-  else
-    puts "nil"
+def fizzbuzz
+  #if
+  #  puts "fizz"
+  #elsif
+  #  puts "buzz"
+  #elsif
+  #  puts "fizzbuzz"
+  #else
+  #  puts "nil"
 end
-
-fizzbuzz(3)
