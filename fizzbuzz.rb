ef339348e339b6017c050f63d7499aefa0aa130f
Ryan Bradley <ryanbradley@kpmg.com>
2019-02-09 23:13:04 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-
-def fizzbuzz()
-  #if
-  #  puts "fizz"
-  #elsif
-  #  puts "buzz"
-  #elsif
-  #  puts "fizzbuzz"
-  #else
-  #  puts "nil"
+def fizzbuzz
 end
+
